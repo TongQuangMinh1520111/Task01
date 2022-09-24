cdd0c4ba5ed43fe5d90dd70b4b81084a39173228
Tong Quang Minh <quangminh1520111@gmail.com>
2022-09-24 23:27:04 +0700

    fix'

--- js/top.js
+++ js/top.js
@@ -1,5 +1,25 @@
 $(document).ready(function () {
   "use strict";
+  var _boxes = $(".overlay");
+  _boxes.bind("lightup", function () {
+    setTimeout(function () {
+      $(".overlay").removeClass("overlay-hide");
+    }, 400);
+  });
+
+  $(".hamburger").click(function (e) {
+    e.preventDefault();
+    $("#header").toggleClass("active");
+    $(this).toggleClass("active");
+    var _this = $(".overlay");
+    _this.toggleClass("overlay-show");
+    if (_this.hasClass("overlay-hide")) {
+      _this.trigger("lightup");
+    } else {
+      $(".overlay").removeClass("overlay-hide");
+      _this.addClass("overlay-hide");
+    }
+  });
 
   $(".product_slider").slick({
     infinite: true,
@@ -8,6 +28,15 @@ $(document).ready(function () {
     slidesToShow: 2,
     slidesToScroll: 4,
     centerMode: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
   $(".special_slider").slick({
     infinite: true,
@@ -16,6 +45,30 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     centerMode: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 491,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
   $(".release_slider").slick({
     infinite: true,
@@ -24,6 +77,30 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     centerMode: false,
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 491,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
   $(".space_slider").slick({
     infinite: true,
@@ -34,17 +111,16 @@ $(document).ready(function () {
     centerMode: false,
   });
   var max = 0;
-  $(".product_slider .product_img").each(function (index) {
+  $(".space_slider figure").each(function (index) {
     max = Math.max(max, $(this).height());
     return max;
   });
-  $(".product_slider .product_img").css("height", `${max}px`);
+  $(".space_slider figure").css("height", `${max}px`);
 
-
-
-  $("#idx_release .release_card figure").matchHeight();
-  $("#idx_release .release_card h4").matchHeight();
-  $("#idx_release .release_card release_txt").matchHeight();
-
-  
+  if ($(window).width() > 490) {
+    $("#idx_special .spec_card h4").matchHeight();
+    $("#idx_release .release_card figure").matchHeight();
+    $("#idx_release .release_card h4").matchHeight();
+    $("#idx_release .release_card release_txt").matchHeight();
+  }
 });
